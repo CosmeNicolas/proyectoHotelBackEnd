33bd615975d3e6e3674e023f2b2e233ce81a4d4a
emir paradi <emir.alejandro.paradi@gmail.com>
2024-03-27 22:36:03 -0300

    se configuro un puerto local para la db

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("desde el back")
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import morgan from "morgan";
+
+import "./src/database/database.js"
+
+//! 1 - Configurar un PUERTO
+const app = express();
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.log("Estoy en el puerto " + app.get("port"));
+});
