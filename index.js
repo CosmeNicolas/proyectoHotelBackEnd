6c3a2130d18cf8cb751240961b62334c7a55a833
emir paradi <emir.alejandro.paradi@gmail.com>
2024-03-27 22:43:58 -0300

    Se configuraron los middlewares en index.js y la rutas de /habitaciones

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 import "./src/database/database.js"
+import habitacionesRouter  from "./src/routes/habitaciones.routes.js"
 
 
 //! 1 - Configurar un PUERTO
@@ -11,3 +14,16 @@ app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
   console.log("Estoy en el puerto " + app.get("port"));
 });
+
+//! 2 - Config MIDDLEWARES
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "/public")));
+
+//! 3 - Config RUTAS
+
+app.use("/api", habitacionesRouter);
---
+++ src/routes/habitaciones.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.route("/habitaciones");
+
+export default router;
