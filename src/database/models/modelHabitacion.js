ae829a02c3b339cb758e8a696d555bede562f283
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-04-02 20:08:47 -0300

    creo el habitacionSchema

---
+++ src/database/models/modelHabitacion.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const habitacionSchema = new Schema({
+  numero: {
+    type: Number,
+    requided: true,
+    min: 100,
+    max: 10000,
+  },
+  tipo: {
+    type: String,
+    required: true,
+    enum: ["Simple", "Doble", "Vip", "Suite"],
+  },
+  precio: {
+    type: Number,
+    requided: true,
+    min: 27000,
+    max: 120000,
+  },
+  fechaIngreso: { type: Date, required: true },
+  
+  fechaSalida: { type: Date, required: true },
+
+  disponible: {
+    type: Boolean,
+    required: true,
+  },
+  imagen: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (valor) => {
+        return /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/.test(valor);
+      },
+      message: (dato) => `${dato.value} no es una URL de imagen valida`,
+    },
+  },
+});
+
+export default mongoose.model("Habitacion", habitacionSchema);
