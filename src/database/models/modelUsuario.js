f06dbcc83f9090b9f71c9a80327626a161cbaeb5
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-04-02 21:23:47 -0300

    creo el UsuarioSchema

---
+++ src/database/models/modelUsuario.js
@@ -0,0 +1,50 @@
+import mongoose, {Schema} from "mongoose";
+
+const usuarioSchema = new Schema({
+  nombreCompleto: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 60,
+  },
+  email: {
+    type: String,
+    required: true,
+    minLength: 10,
+    maxLength: 340,
+    validate: {
+      validator: (valor) => {
+        const pattern =
+          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        return pattern.test(valor);
+      },
+      message: (props) => `${props.value} No es un E-mail válido`,
+    },
+    unique: true,
+  },
+  usuario: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 20,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  rol: {
+    type: String,
+    required: true,
+    enum: ["Administrador", "Usuario"],
+  },
+
+  suspendido: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Usuario =  mongoose.model('usuario', usuarioSchema)
+
+export default Usuario;
