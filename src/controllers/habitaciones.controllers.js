0adb1ae5ef9a4ecb9191eeb25a39c6b64a113e9e
emir paradi <emir.alejandro.paradi@gmail.com>
2024-04-04 11:19:11 -0300

    Se realizo la funcion para, obtener 1 habitacion mediante su id

--- src/controllers/habitaciones.controllers.js
+++ src/controllers/habitaciones.controllers.js
@@ -1,23 +1,44 @@
-// falta crear el modelHabitacion e importarlo
 import Habitacion from "../database/models/modelHabitacion.js";
+
 //! 1 - GET para listar todas las habitaciones
-export const listarHabitaciones = async(req, res) => {
+export const listarHabitaciones = async (req, res) => {
   try {
     const habitaciones = await Habitacion.find();
-    res.status(200).json(habitaciones)
+    res.status(200).json(habitaciones);
   } catch (error) {
     console.error(error);
-    res.status(500).json({mensaje: 'Erorr al buscar la habitacion'})
+    res.status(500).json({ mensaje: "Erorr al buscar la habitacion" });
   }
 };
 
-export const crearHabitacion = async(req, res)=>{
+//! 3 - GET  de 1 habitación por id
+export const obtenerHabitacion = async (req, res) => {
+  try {
+    console.log(req.params.id)
+    const habitacionBuscada = await Habitacion.findById(req.params.id);
+    if (habitacionBuscada === null) {
+      return res.status(404).json({
+        mensaje: "La habitacion con el id enviado no existe",
+      });
+    }
+    res.status(200).json(habitacionBuscada);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      mensaje: "No se pudo encontrar la habitacion solicitada, id erróneo",
+    });
+  }
+};
+
+
+//! 2 - POST Crear nueva habitación
+export const crearHabitacion = async (req, res) => {
   try {
     const nuevaHabitacion = new Habitacion(req.body);
     await nuevaHabitacion.save();
-    res.status(201).json({mensaje:'La Habitacion fue creada con Éxito!'})
+    res.status(201).json({ mensaje: "La Habitacion fue creada con Éxito!" });
   } catch (error) {
-    console.log(error)
-    res.status(400).json({mensaje:'La Habitacion no pudo ser creada'})
+    console.log(error);
+    res.status(400).json({ mensaje: "La Habitacion no pudo ser creada" });
   }
-}
+};
--- src/routes/habitaciones.routes.js
+++ src/routes/habitaciones.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { listarHabitaciones, crearHabitacion } from "../controllers/habitaciones.controllers.js";
+import { listarHabitaciones, crearHabitacion, obtenerHabitacion } from "../controllers/habitaciones.controllers.js";
 
 
 const router = Router();
 
 router.route("/habitaciones").get(listarHabitaciones).post(crearHabitacion)
+router.route("/habitaciones/:id").get(obtenerHabitacion)
 
 export default router;
