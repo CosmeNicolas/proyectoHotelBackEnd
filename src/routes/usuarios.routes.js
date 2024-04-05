074455e96d7c0f7440a278b4c5b2a20f5c152e3e
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-04-05 19:49:49 -0300

    creo las rutas para obtenerusuario y editarusuario

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -6,5 +6,6 @@ const router = Router()
 
 router.route("/registrar").post(crearUsuario)
 router.route("/usuarios").get(listarUsuarios)
+router.route("/usuarios/:id").get(obtenerUsuario).put(editarUsuario)
 router.route("/").post(login)
 export default router
