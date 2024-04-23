34db1d841a7ef5fa6ee70f38c3d816d2d2510dab
emir paradi <emir.alejandro.paradi@gmail.com>
2024-04-23 07:44:28 -0300

    Se agrego un nuevo controlador y ruta para manejar la reserva desde el usuario sin necesidad de token

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,12 +1,24 @@
 import { Router } from "express";
-import { crearUsuario, login , listarUsuarios, obtenerUsuario, editarUsuario, eliminarUsuario} from "../controllers/usuarios.controllers.js";
+import {
+  crearUsuario,
+  login,
+  listarUsuarios,
+  obtenerUsuario,
+  editarUsuario,
+  eliminarUsuario,
+} from "../controllers/usuarios.controllers.js";
 import validacionUsuario from "../helpers/validacionUsuario.js";
+import { reservarHabitacion } from "../controllers/reservas.controllers.js";
 
+const router = Router();
 
-const router = Router()
-
-router.route("/registrar").post([validacionUsuario],crearUsuario)
-router.route("/usuarios").get(listarUsuarios)
-router.route("/usuarios/:id").get(obtenerUsuario).put(editarUsuario).delete(eliminarUsuario)
-router.route("/").post(login)
-export default router
+router.route("/usuarios/reservar/:id").put(reservarHabitacion);
+router.route("/registrar").post([validacionUsuario], crearUsuario);
+router.route("/usuarios").get(listarUsuarios);
+router
+  .route("/usuarios/:id")
+  .get(obtenerUsuario)
+  .put(editarUsuario)
+  .delete(eliminarUsuario);
+router.route("/").post(login);
+export default router;
